b7d52a48a80726b6bda75f5c9c543d66c01c3178
Aumimul Ahoshan <ahoshanaumimul@gmail.com>
2025-12-26 20:59:47 +0600

    Add files via upload

---
+++ dart.dart
@@ -0,0 +1,12 @@
+void main(){
+  Set<int> A={1,2,3,4,5};
+  Set<int> B={4,5,6,7,8};
+  print("The elements in Set A is:");
+  for(int item in  A){
+    print(item);
+  }
+  print("The elements in Set B is:");
+  for(int item in B){
+    print(item);
+  }
+}
